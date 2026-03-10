6520efd6dc20a0ad9efb05bdce15d40ca7ecbef0
Léo Leman <leo.leman555@gmail.com>
2026-03-10 18:41:21 +0100

    chore(hardware): remove debug button used to play an audio fil

--- firmware/src/start.py
+++ firmware/src/start.py
@@ -1,60 +1,9 @@
-# start.py
 import time
-import _thread
-from machine import Pin
-
 from ble import BleService
 from audio import AudioPlayer
 from storage import Storage
 from rtc import TimeRead
 from scheduler import MemoScheduler
-
-
-class Button:
-    """Physical button handler."""
-
-    def __init__(self, pin, callback, pullup=True):
-        self.button = Pin(
-            pin,
-            Pin.IN,
-            Pin.PULL_UP if pullup else None
-        )
-        self.callback = callback
-        self.last_state = 1
-
-    def poll(self):
-        state = self.button.value()
-        if self.last_state == 1 and state == 0:
-            self.callback()
-            time.sleep(0.2)
-        self.last_state = state
-
-
-class Controller:
-    """High-level user interaction controller."""
-
-    def __init__(self, audio: AudioPlayer, storage: Storage):
-        self.audio = audio
-        self.storage = storage
-        self.track = "{}/audio/received.wav".format(storage.root)
-
-    def on_button_pressed(self):
-        if not self.audio or not self.audio.available:
-            print("[CTRL] Audio unavailable")
-            return
-
-        if not self.audio.is_playing():
-            print("[CTRL] Play", self.track)
-            _thread.start_new_thread(
-                self.audio.play_wav,
-                (self.track,)
-            )
-            return
-
-        if self.audio.is_paused():
-            self.audio.resume()
-        else:
-            self.audio.pause()
 
 
 def main():
@@ -71,17 +20,11 @@ def main():
 
     ble = BleService(storage)
     rtc = TimeRead()
-
-    controller = Controller(audio, storage)
-    button = Button(pin=15, callback=controller.on_button_pressed)
     scheduler = MemoScheduler(rtc, storage, audio)
 
     print("[START] Ready")
 
     while True:
-        # Poll hardware button
-        button.poll()
-
         # Run scheduler
         scheduler.tick()
 
